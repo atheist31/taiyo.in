f5bb11228b6487c1579d1e33fea0315265d37f5d
atheist31 <rkb080@gmail.com>
2023-08-19 10:24:58 +0530

    at3

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -5,7 +5,12 @@ const Homepage = () => {
   const navigate = useNavigate();
 
   return (
+    <div>
+    <div className="text-center">
+        <h1 className="text-4xl bg-yellow-700 font-semibold text-white">Welcome to the Taiyo.AI</h1>
+        </div>
     <div className="bg-yellow-700 min-h-screen flex md:flex-row justify-center items-center flex-col gap-5">
+       
       <button
         onClick={() => navigate("/add-contact")}
         className="text-white bg-red-500 border border-black-600 hover:bg-red-600 py-3 px-6 rounded-lg transition duration-300 focus:outline-none"
@@ -19,6 +24,8 @@ const Homepage = () => {
         Chart App
       </button>
     </div>
+    </div>
+    
   );
 };
 
